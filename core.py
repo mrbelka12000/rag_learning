650fee7db177ab3e574081166e493aa1038d8638
beknur.karshyga <beknur.karshyga@armenotech.com>
2025-08-30 04:15:33 +0500

    handle files from data dir

--- core.py
+++ core.py
@@ -3,21 +3,28 @@ from os import listdir
 from embedings import load_pdf, load_text, get_embedding_function, evaluate_embedding
 from vectors import make_vectors
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from typing import List
+
 
 def run_vectorization(path="data"):
     onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-    pages = []
+    pages: List[Document] = []
     for file in onlyfiles:
+        fp = join(path, file)
         if file.endswith(".pdf"):
-            pages = load_pdf(join(path, file))
-        
+            pages.extend(load_pdf(fp))
+
         elif file.endswith(".txt"):
-            pages = load_text(join(path, file))
+            pages.extend(load_text(fp))
+
         else:
             print(f"Unsupported file format: {file}")
     
+    if not pages:
+        raise ValueError(f"No supported files found in {path}")
+
     # Split into chunks
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500, 
